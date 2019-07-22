cd9a0e84bd91bd19ad5ecd194f1c45de36b150f3
Jefersson Gálvez <jheans_79a@hotmail.com>
2019-07-21 21:58:40 -0500

    Se agrega APK al repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  URL_CURRENCY_CONVERTER: 'http://data.fixer.io/api/latest',
+  KEY_URL_CURRENCY_CONVERTER: '33b23d6e01efe285daf21f65e1124757'
 };
